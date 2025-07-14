f22e3e42e43ed30cb2cd30f47b3210f231ba37d9
Elyes Bali <100534870+Elyes-Bali@users.noreply.github.com>
2025-07-14 01:51:52 +0100

    Update Application.java

--- src/main/java/com/example/project/backend/Application.java
+++ src/main/java/com/example/project/backend/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @EnableScheduling
@@ -19,9 +17,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-   	 @Bean
-   	 public BCryptPasswordEncoder passwordEncoder() {
-       	  return new BCryptPasswordEncoder();
-    }
 
 }
